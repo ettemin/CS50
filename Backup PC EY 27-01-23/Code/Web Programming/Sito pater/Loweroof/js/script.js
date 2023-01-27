5e9d424614f4d5d0e3c5f6f5be49d5db0c33bde5
ettemin <80461511+ettemin@users.noreply.github.com>
2023-01-27 10:55:36 +0100

    Add files via upload

---
+++ Backup PC EY 27-01-23/Code/Web Programming/Sito pater/Loweroof/js/script.js
@@ -0,0 +1,100 @@
+
+
+(() =>{
+ 
+  const openNavMenu = document.querySelector(".open-nav-menu"),
+  closeNavMenu = document.querySelector(".close-nav-menu"),
+  navMenu = document.querySelector(".nav-menu"),
+  menuOverlay = document.querySelector(".menu-overlay"),
+  mediaSize = 991;
+
+  openNavMenu.addEventListener("click", toggleNav);
+  closeNavMenu.addEventListener("click", toggleNav);
+  // close the navMenu by clicking outside
+  menuOverlay.addEventListener("click", toggleNav);
+
+  function toggleNav() {
+  	navMenu.classList.toggle("open");
+  	menuOverlay.classList.toggle("active");
+  	document.body.classList.toggle("hidden-scrolling");
+  }
+
+  navMenu.addEventListener("click", (event) =>{
+      if(event.target.hasAttribute("data-toggle") && 
+      	window.innerWidth <= mediaSize){
+      	// prevent default anchor click behavior
+      	event.preventDefault();
+      	const menuItemHasChildren = event.target.parentElement;
+        // if menuItemHasChildren is already expanded, collapse it
+        if(menuItemHasChildren.classList.contains("active")){
+        	collapseSubMenu();
+        }
+        else{
+          // collapse existing expanded menuItemHasChildren
+          if(navMenu.querySelector(".menu-item-has-children.active")){
+        	collapseSubMenu();
+          }
+          // expand new menuItemHasChildren
+          menuItemHasChildren.classList.add("active");
+          const subMenu = menuItemHasChildren.querySelector(".sub-menu");
+          subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+        }
+      }
+  });
+  function collapseSubMenu(){
+  	navMenu.querySelector(".menu-item-has-children.active .sub-menu")
+  	.removeAttribute("style");
+  	navMenu.querySelector(".menu-item-has-children.active")
+  	.classList.remove("active");
+  }
+  function resizeFix(){
+  	 // if navMenu is open ,close it
+  	 if(navMenu.classList.contains("open")){
+  	 	toggleNav();
+  	 }
+  	 // if menuItemHasChildren is expanded , collapse it
+  	 if(navMenu.querySelector(".menu-item-has-children.active")){
+        	collapseSubMenu();
+     }
+  }
+
+  window.addEventListener("resize", function(){
+     if(this.innerWidth > mediaSize){
+     	resizeFix();
+     }
+  });
+
+})();
+
+// Load the Visualization API and the corechart package.
+google.charts.load('current', {'packages':['corechart']});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawChart);
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart() {
+
+  // Create the data table.
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Topping');
+  data.addColumn('number', 'Slices');
+  data.addRows([
+    ['Italy', 2299],
+    ['Spain', 2616],
+    ['Netherlands', 3949],
+    ['Germany', 4600],
+    ['France', 4639],
+    ['UK', 4905]
+  ]);
+
+  // Set chart options
+  var options = {'width': 500,
+                 'height':300};
+
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
